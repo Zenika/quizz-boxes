b7c95af0c7e240a9efca07b5f2936dfa75d40d10
Nicolas Payot <nicolas.payot@gmail.com>
2015-07-30 14:28:30 +0200

    test(quizz): add tests to quizz controller spec

--- implems/AngularJS-ES6/src/app/quizz/quizz.controller.spec.js
+++ implems/AngularJS-ES6/src/app/quizz/quizz.controller.spec.js
@@ -8,16 +8,34 @@ describe('controller: QuizzController', function() {
 
   beforeEach(inject(function($controller) {
     quizzController = $controller('QuizzController');
-    quizzController.questions = [{
-      label: 'question?',
-      answers: [
-        { id: 1, label: 'answer 1', valid: true },
-        { id: 2, label: 'answer 2', valid: false },
-        { id: 3, label: 'answer 3', valid: true },
-        { id: 4, label: 'answer 4', valid: false }
-      ]
-    }];
-    quizzController.qIndex = 0;
+    quizzController.questions = [
+      {
+        label: 'question1?',
+        answers: [
+          { id: 1, label: 'answer 1', valid: true },
+          { id: 2, label: 'answer 2', valid: false },
+          { id: 3, label: 'answer 3', valid: true },
+          { id: 4, label: 'answer 4', valid: false }
+        ]
+      },
+      {
+        label: 'question2?',
+        answers: [
+          { id: 1, label: 'answer 1', valid: false },
+          { id: 2, label: 'answer 2', valid: true },
+          { id: 3, label: 'answer 3', valid: false }
+        ]
+      },
+      {
+        label: 'question3?',
+        answers: [
+          { id: 1, label: 'answer 1', valid: true },
+          { id: 2, label: 'answer 2', valid: true },
+          { id: 3, label: 'answer 3', valid: true },
+          { id: 4, label: 'answer 4', valid: false }
+        ]
+      }
+    ];
   }));
 
   it('should show success toastr when user\'s current answer is valid', function() {
@@ -46,4 +64,29 @@ describe('controller: QuizzController', function() {
     quizzController.nextQuestion();
     expect(quizzController.toastr.error).toHaveBeenCalled();
   });
+
+  it('should have a result of X valid answers when user\'s answer are all valid', inject(function($timeout) {
+    expect(quizzController.validAnswersCount).toBe(0);
+
+    quizzController.currentAnswer = { 1: true, 3: true };
+    quizzController.nextQuestion();
+    $timeout.flush();
+    quizzController.currentAnswer = { 2: true };
+    quizzController.nextQuestion();
+    $timeout.flush();
+    quizzController.currentAnswer = { 1: true, 2: true, 3: true };
+    quizzController.nextQuestion();
+    $timeout.flush();
+
+    expect(quizzController.validAnswersCount).toBe(3);
+  }));
+
+  it('should reset valid answers count when quizz has reset', inject(function($timeout) {
+    quizzController.currentAnswer = { 1: true, 3: true };
+    quizzController.nextQuestion();
+    $timeout.flush();
+    expect(quizzController.validAnswersCount).toBe(1);
+    quizzController.reset();
+    expect(quizzController.validAnswersCount).toBe(0);
+  }));
 });
